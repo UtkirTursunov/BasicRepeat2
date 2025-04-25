c081c6c05389eccd90663004cef3f2adb32f77c6
Utkir <utkir-0710@mail.ru>
2025-04-25 15:50:30 +0500

    Add files via upload

---
+++ BasicRepeat2/Methods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRepeat2;
+
+public class Methods
+{
+   public static void MainScreen()
+    {
+        Console.WriteLine("1. Add a task.");
+        Console.WriteLine("2. Remove a task.");
+        Console.WriteLine("3. Mark the task as completed.");
+        Console.WriteLine("4. Display tasks.");
+        Console.WriteLine("5. Exit");
+    }
+
+    public static void AddTask(List<(string Task, bool IsCompleted)> tasks)
+    {
+        Console.WriteLine("Enter the task!");
+        string Task = Console.ReadLine();
+        tasks.Add((Task, false));
+        Console.WriteLine("Task added successfully.");
+    }
+
+    public static void RemoveTask(List<(string Task, bool IsCompleted)> tasks)
+    {
+        DisplayTasks(tasks);
+        Console.WriteLine("Choose the number of task for removing...");
+        int.TryParse(Console.ReadLine(), out int index);
+
+        if (index > 0 && index <= tasks.Count)
+        {
+            tasks.RemoveAt(index - 1);
+            Console.WriteLine("Task removed successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid task number...Please check the number of task");
+        }
+    }
+
+    public static void MarkAsCompleted(List<(string Task, bool IsCompleted)> tasks)
+    {
+        DisplayTasks(tasks);
+        Console.WriteLine("Choose the number of task for marking as completed...");
+        int.TryParse(Console.ReadLine(), out int index);
+
+        if(index > 0 && index <= tasks.Count)
+        {
+            tasks[index - 1] = (tasks[index - 1].Task, true);
+            Console.WriteLine("Task has been marked as completed");
+        }
+        else
+        {
+            Console.WriteLine("Invalid task number...Please check the number of task");
+        }
+    }
+
+    public static void DisplayTasks(List<(string Task, bool IsCompleted)> tasks)
+    {
+        if(tasks.Count == 0)
+        {
+            Console.WriteLine("No found");
+        }
+        else
+        {
+            Console.WriteLine("\nTasks:");
+            for(int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tasks[i].Task} - {(tasks[i].IsCompleted ? "Completed" : "Not Completed")}");
+            }
+        }
+    }
+}
---
+++ BasicRepeat2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace BasicRepeat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string Task, bool IsCompleted)> tasks = new List<(string, bool)>();
+            bool isRun = true;
+
+            Methods.MainScreen();
+
+            while (isRun)
+            {
+                int.TryParse(Console.ReadLine(), out int choice);
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Methods.AddTask(tasks);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Methods.RemoveTask(tasks);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Methods.MarkAsCompleted(tasks);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Methods.DisplayTasks(tasks);
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Program exited. Goodbye!");
+                        isRun = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again");
+                        break;
+                }
+
+                if (choice >= 1 && choice <= 4)
+                {
+                    Console.WriteLine("\nDo you want any operation? (yes/no)");
+                    string continueChoice = Console.ReadLine()?.ToLower();
+
+                    if (continueChoice == "yes" || continueChoice == "y")
+                    {
+                        Console.Clear();
+                        Methods.MainScreen();
+                    }
+                    else if (continueChoice == "no" || continueChoice == "n")
+                    {
+                        isRun = false;
+                        Console.Clear();
+                        Console.WriteLine("Program excited. Goodbye!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Return to the main menu.");
+                        Thread.Sleep(1500);
+                        Console.Clear();
+                        Methods.MainScreen();
+                    }
+                }
+            }
+        }
+    }
+}
